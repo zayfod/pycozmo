4b38d9151d23d0c5abceb26c0fe41d5a0ca9203b
Kaloyan Tenchov <zayfod@gmail.com>
2019-09-22 10:48:45 -0400

    Initial face rendering experiment

--- pycozmo/anim.py
+++ pycozmo/anim.py
@@ -8,7 +8,9 @@ Animation data structures are declared in FlatBuffers format in files/cozmo/cozm
 
 """
 
-from typing import List
+from typing import List, Tuple
+
+from PIL import Image, ImageDraw
 
 from . import CozmoAnim
 from . import protocol_encoder
@@ -28,6 +30,38 @@ class AnimKeyframe(object):
         self.record_heading = False
         self.face_animation = None
         self.event_id = None    # DEVICE_AUDIO_TRIGGER / ENERGY_DRAINCUBE_END / TAPPED_BLOCK
+
+
+class AnimEye(object):
+
+    def __init__(self,
+                 center: Tuple[float, float] = (10.0, 0.0),
+                 radius: Tuple[float, float] = (1.22, 0.9),
+                 unknown4: float = 0.0,
+                 curv1: Tuple[float, float, float, float] = (0.5, 0.5, 0.5, 0.5),
+                 curv2: Tuple[float, float, float, float] = (0.5, 0.5, 0.5, 0.5),
+                 unknown13: Tuple[float, float, float, float, float, float] = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0)):
+        self.center = center
+        self.radius = radius
+        self.unknown4 = unknown4
+        self.curv1 = curv1
+        self.curv2 = curv2
+        self.unknown13 = unknown13
+
+
+class AnimFace(object):
+
+    def __init__(self,
+                 angle: float = 0.0,
+                 center: Tuple[float, float] = (0.0, 0.0),
+                 scale: Tuple[float, float] = (0.0, 0.0),
+                 left_eye: AnimEye = AnimEye(),
+                 right_eye: AnimEye = AnimEye()):
+        self.angle = angle
+        self.center = center
+        self.scale = scale
+        self.left_eye = left_eye
+        self.right_eye = right_eye
 
 
 class AnimClip(object):
@@ -53,9 +87,36 @@ class AnimClip(object):
         # TODO
         pass
 
+    def face(self, trigger_time: int, face: AnimFace) -> None:
+        # TODO
+        pass
+
+
+def render_face(face: AnimFace) -> None:
+    CX = 63
+    CY = 31
+    SX = 2.25
+    SY = 2.25
+    RX = 15
+    RY = 20
+    im = Image.new("1", (128, 64), color=(0))
+    draw = ImageDraw.Draw(im)
+
+    # draw.line((0, 0) + im.size, fill=128)
+    # draw.line((0, im.size[1], im.size[0], 0), fill=128)
+
+    for eye in (face.left_eye, face.right_eye):
+        x1 = CX + SX * eye.center[0] - RX * eye.radius[0]
+        y1 = CY + SY * eye.center[1] - RY * eye.radius[1]
+        x2 = CX + SX * eye.center[0] + RX * eye.radius[0]
+        y2 = CY + SY * eye.center[1] + RY * eye.radius[1]
+        draw.ellipse(xy=((x1, y1), (x2, y2)), fill=1, outline=1, width=1)
+
+    im.show()
+    pass
 
 def load_anim_clip(fbclip: CozmoAnim.AnimClip) -> AnimClip:
-    """ Convert a single Cozmo FlatBuggers animation clip into a PyCozmo AnimClip object. """
+    """ Convert a single Cozmo FlatBuffers animation clip into a PyCozmo AnimClip object. """
     clip = AnimClip(fbclip.Name().decode("utf-8"))
     fbkfs = fbclip.Keyframes()
 
@@ -146,17 +207,29 @@ def load_anim_clip(fbclip: CozmoAnim.AnimClip) -> AnimClip:
         fbkf = fbkfs.ProceduralFaceKeyFrame(i)
         # TODO
         trigger_time = fbkf.TriggerTimeMs()
-        face_angle = fbkf.FaceAngle()
-        face_center_x = fbkf.FaceCenterX()
-        face_center_y = fbkf.FaceCenterY()
-        face_scale_x = fbkf.FaceScaleX()
-        face_scale_y = fbkf.FaceScaleY()
-        left_eye = []
-        for j in range(fbkf.LeftEyeLength()):
-            left_eye.append(fbkf.LeftEye(j))
-        right_eye = []
-        for j in range(fbkf.RightEyeLength()):
-            right_eye.append(fbkf.RightEye(j))
+        assert fbkf.LeftEyeLength() == 19
+        left_eye = AnimEye(center=(fbkf.LeftEye(0), fbkf.LeftEye(1)),
+                           radius=(fbkf.LeftEye(2), fbkf.LeftEye(3)),
+                           unknown4=fbkf.LeftEye(4),
+                           curv1=(fbkf.LeftEye(5), fbkf.LeftEye(6), fbkf.LeftEye(7), fbkf.LeftEye(8)),
+                           curv2=(fbkf.LeftEye(9), fbkf.LeftEye(10), fbkf.LeftEye(11), fbkf.LeftEye(12)),
+                           unknown13=(fbkf.LeftEye(13), fbkf.LeftEye(14), fbkf.LeftEye(15),
+                                      fbkf.LeftEye(16), fbkf.LeftEye(17), fbkf.LeftEye(18)))
+        assert fbkf.RightEyeLength() == 19
+        right_eye = AnimEye(center=(fbkf.RightEye(0), fbkf.RightEye(1)),
+                           radius=(fbkf.RightEye(2), fbkf.RightEye(3)),
+                           unknown4=fbkf.RightEye(4),
+                           curv1=(fbkf.RightEye(5), fbkf.RightEye(6), fbkf.RightEye(7), fbkf.RightEye(8)),
+                           curv2=(fbkf.RightEye(9), fbkf.RightEye(10), fbkf.RightEye(11), fbkf.RightEye(12)),
+                           unknown13=(fbkf.RightEye(13), fbkf.RightEye(14), fbkf.RightEye(15),
+                                      fbkf.RightEye(16), fbkf.RightEye(17), fbkf.RightEye(18)))
+        face = AnimFace(angle=fbkf.FaceAngle(),
+                        center=(fbkf.FaceCenterX(), fbkf.FaceCenterY()),
+                        scale=(fbkf.FaceScaleX(), fbkf.FaceScaleY()),
+                        left_eye=left_eye,
+                        right_eye=right_eye)
+        clip.face(trigger_time, face)
+        render_face(face)
 
     # Convert RobotAudio key frames to messages
     for i in range(fbkfs.RobotAudioKeyFrameLength()):
