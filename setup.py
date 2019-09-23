eb58b5905afde863ba83dd4e89daf00214a1bb6e
Kaloyan Tenchov <zayfod@gmail.com>
2019-09-22 20:24:39 -0400

    Clarify Pillow requirements

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setuptools.setup(
     author_email="zayfod@gmail.com",
     url="https://github.com/zayfod/pycozmo/",
     python_requires=">=3.5.4",
-    install_requires=["dpkt", "numpy", "Pillow", "flatbuffers"],
+    install_requires=["dpkt", "numpy", "Pillow>=6.0.0", "flatbuffers"],
     keywords=["anki", "cozmo", "robot", "robotics"],
     classifiers=[
         "Development Status :: 3 - Alpha",
