7c4be06530a50ab7bda24878064b0aa1d5141d1d
Kaloyan Tenchov <zayfod@gmail.com>
2019-10-08 09:27:09 -0400

    Improve image encoder

--- pycozmo/image_encoder.py
+++ pycozmo/image_encoder.py
@@ -180,7 +180,7 @@ class ImageEncoder(object):
         if color:
             # Draw
             if cnt <= 15:
-                cmd = 0x80 + (cnt << 2) + 0x01
+                cmd = 0x80 + (cnt << 2) + 0x02
             else:
                 assert 15 <= cnt <= 31
                 cmd = 0xc0 + ((cnt - 16) << 2) + 0x02
--- pycozmo/tests/test_image_encoder.py
+++ pycozmo/tests/test_image_encoder.py
@@ -51,7 +51,6 @@ class TestImageEncoder(unittest.TestCase):
         fixture = FIXTURES["top_left"]
         sim = fixture["image"]
         expected = fixture["seq"]
-        # TODO: Cozmo does 82
         actual = self._encode(sim)
         self.assertEqual(expected, actual)
         self.assertSameImage(sim, actual)
@@ -60,7 +59,6 @@ class TestImageEncoder(unittest.TestCase):
         fixture = FIXTURES["top_left_5"]
         sim = fixture["image"]
         expected = fixture["seq"]
-        # TODO: Cozmo does 82
         actual = self._encode(sim)
         self.assertEqual(expected, actual)
         self.assertSameImage(sim, actual)
@@ -85,7 +83,6 @@ class TestImageEncoder(unittest.TestCase):
         fixture = FIXTURES["top_line"]
         sim = fixture["image"]
         expected = fixture["seq"]
-        # TODO: Cozmo does "82:7f:7e". Why 82???
         actual = self._encode(sim)
         self.assertEqual(expected, actual)
         self.assertSameImage(sim, actual)
@@ -94,7 +91,6 @@ class TestImageEncoder(unittest.TestCase):
         fixture = FIXTURES["bottom_line"]
         sim = fixture["image"]
         expected = fixture["seq"]
-        # TODO: Cozmo does "f8:82:7f:7e". Why 82???
         actual = self._encode(sim)
         self.assertEqual(expected, actual)
         self.assertSameImage(sim, actual)
@@ -119,7 +115,6 @@ class TestImageEncoder(unittest.TestCase):
         fixture = FIXTURES["columns"]
         sim = fixture["image"]
         expected = fixture["seq"]
-        # TODO: Cozmo does fd. Why?
         actual = self._encode(sim)
         self.assertEqual(expected, actual)
         self.assertSameImage(sim, actual)
@@ -128,8 +123,6 @@ class TestImageEncoder(unittest.TestCase):
         fixture = FIXTURES["rect"]
         sim = fixture["image"]
         expected = fixture["seq"]
-        # TODO: Cozmo does:
-        # expected = "fe:82:f4:82:7f:7c:fe"
         actual = self._encode(sim)
         self.assertEqual(expected, actual)
         self.assertSameImage(sim, actual)
@@ -138,8 +131,6 @@ class TestImageEncoder(unittest.TestCase):
         fixture = FIXTURES["rect2"]
         sim = fixture["image"]
         expected = fixture["seq"]
-        # TODO: Cozmo does:
-        # expected = "00:fe:82:f4:82:7f:7a:fe:00"
         actual = self._encode(sim)
         self.assertEqual(expected, actual)
         self.assertSameImage(sim, actual)
@@ -148,8 +139,6 @@ class TestImageEncoder(unittest.TestCase):
         fixture = FIXTURES["rect3"]
         sim = fixture["image"]
         expected = fixture["seq"]
-        # TODO: Cozmo does:
-        # expected = "01:80:f6:80:80:82:ec:82:7f:78:80:f6:01"
         actual = self._encode(sim)
         self.assertEqual(expected, actual)
         self.assertSameImage(sim, actual)
@@ -158,8 +147,6 @@ class TestImageEncoder(unittest.TestCase):
         fixture = FIXTURES["rect4"]
         sim = fixture["image"]
         expected = fixture["seq"]
-        # TODO: Cozmo does
-        # expected = "fe:82:f4:82:fe:86:ec:86:7f:78:fe:82:f4:82:fe"
         actual = self._encode(sim)
         self.assertEqual(expected, actual)
         self.assertSameImage(sim, actual)
@@ -168,10 +155,6 @@ class TestImageEncoder(unittest.TestCase):
         fixture = FIXTURES["diagonal"]
         sim = fixture["image"]
         expected = fixture["seq"]
-        # TODO: Cozmo does
-        # expected = "82:42:80:82:42:84:82:42:88:82:42:8c:82:42:90:82:42:94:82:42:98:82:42:9c:82:42:a0:82:42:a4:82:42:" \
-        #            "a8:82:42:ac:82:42:b0:82:42:b4:82:42:b8:82:42:bc:82:42:c0:82:42:c4:82:42:c8:82:42:cc:82:42:d0:82:" \
-        #            "42:d4:82:42:d8:82:42:dc:82:42:e0:82:42:e4:82:42:e8:82:42:ec:82:42:f0:82:42:f4:82:42:f8:82:42"
         actual = self._encode(sim)
         self.assertEqual(expected, actual)
         self.assertSameImage(sim, actual)
@@ -180,10 +163,6 @@ class TestImageEncoder(unittest.TestCase):
         fixture = FIXTURES["diagonal2"]
         sim = fixture["image"]
         expected = fixture["seq"]
-        # TODO: Cozmo does:
-        # expected = "f8:82:42:f4:82:42:f0:82:42:ec:82:42:e8:82:42:e4:82:42:e0:82:42:dc:82:42:d8:82:42:d4:82:42:d0:82:" \
-        #            "42:cc:82:42:c8:82:42:c4:82:42:c0:82:42:bc:82:42:b8:82:42:b4:82:42:b0:82:42:ac:82:42:a8:82:42:a4:" \
-        #            "82:42:a0:82:42:9c:82:42:98:82:42:94:82:42:90:82:42:8c:82:42:88:82:42:84:82:42:80:82:42:82:42"
         actual = self._encode(sim)
         self.assertEqual(expected, actual)
         self.assertSameImage(sim, actual)
