247b2df290bf6a5b6026aa6efe6312a22287a89b
Kaloyan Tenchov <zayfod@gmail.com>
2019-10-22 09:19:05 -0400

    Tune face rendering and add an example

--- pycozmo/procedural_face.py
+++ pycozmo/procedural_face.py
@@ -56,11 +56,15 @@ class ProceduralLid(object):
 
 class ProceduralEye(object):
 
-    WIDTH = 25
-    HEIGHT = WIDTH
-    CORNER_RADIUS = 5
+    WIDTH = 28
+    HEIGHT = 40
+    CORNER_RADIUS = 8
+    X_FACTOR = 0.55
+    Y_FACTOR = 0.25
+    RESAMPLE = Image.NEAREST
 
     def __init__(self):
+        self.offset = 0
         self.center = [0, 0]
         self.scale = [1.0, 1.0]
         self.angle = 0.0
@@ -69,8 +73,6 @@ class ProceduralEye(object):
         self.upper_inner_radius = [0.5, 0.5]
         self.upper_outer_radius = [0.5, 0.5]
         self.lid = [ProceduralLid(), ProceduralLid()]
-        self.resample = Image.NEAREST
-        self.debug = True
 
     def render(self, im: Image) -> None:
         # Eye image
@@ -86,18 +88,20 @@ class ProceduralEye(object):
         rounded_rectangle(draw, ((x1, y1), (x2, y2)), self.CORNER_RADIUS, fill=1)
 
         # Draw lids
-        for lid in self.lid:
-            lid.render(eye)
+        self.lid[0].render(eye)
+        self.lid[1].render(eye)
 
         # Rotate
-        eye = eye.rotate(self.angle, resample=self.resample, expand=1)
+        eye = eye.rotate(self.angle, resample=self.RESAMPLE, expand=1)
 
         # Scale
-        scale = (int(float(eye.size[0]) * self.scale[0]), int(float(eye.size[1] * self.scale[1])))
-        eye = eye.resize(scale, resample=self.resample)
+        scale = (int(float(eye.size[0]) * self.scale[0]),
+                 int(float(eye.size[1]) * self.scale[1]))
+        eye = eye.resize(scale, resample=self.RESAMPLE)
 
         # Translate and compose
-        location = ((im.size[0] - eye.size[0]) // 2 + self.center[0], (im.size[1] - eye.size[1]) // 2)
+        location = ((im.size[0] - eye.size[0]) // 2 + int(self.center[0] * self.X_FACTOR) + self.offset,
+                    (im.size[1] - eye.size[1]) // 2 + int(self.center[1] * self.Y_FACTOR))
         im.paste(eye, location, eye)
 
 
@@ -107,15 +111,19 @@ class ProceduralFace(object):
     HEIGHT = 64
     CENTER_X = WIDTH // 2
     CENTER_Y = HEIGHT // 2
+    X_FACTOR = 0.55
+    Y_FACTOR = 0.25
+    LEFT_EYE_OFFSET = -23
+    RIGHT_EYE_OFFSET = 21
+    RESAMPLE = Image.NEAREST
 
     def __init__(self):
         self.center = (0, 0)
         self.scale = (1.0, 1.0)
         self.angle = 0.0
         self.eyes = [ProceduralEye(), ProceduralEye()]
-        self.eyes[0].center[0] = 20
-        self.eyes[1].center[0] = -20
-        self.resample = Image.NEAREST
+        self.eyes[0].offset = self.LEFT_EYE_OFFSET
+        self.eyes[1].offset = self.RIGHT_EYE_OFFSET
         self.debug = False
 
     def render(self) -> Image:
@@ -130,18 +138,20 @@ class ProceduralFace(object):
             debug_box(face)
 
         # Draw eyes
-        for eye in self.eyes:
-            eye.render(face)
+        self.eyes[0].render(face)
+        self.eyes[1].render(face)
 
         # Rotate
-        face = face.rotate(self.angle, resample=self.resample, expand=1)
+        face = face.rotate(self.angle, resample=self.RESAMPLE, expand=1)
 
         # Scale
-        scale = (int(float(face.size[0]) * self.scale[0]), int(float(face.size[1] * self.scale[1])))
-        face = face.resize(scale, resample=self.resample)
+        scale = (int(float(face.size[0]) * self.scale[0]),
+                 int(float(face.size[1] * self.scale[1])))
+        face = face.resize(scale, resample=self.RESAMPLE)
 
         # Translate and compose
-        location = ((im.size[0] - face.size[0]) // 2 + self.center[0], (im.size[1] - face.size[1]) // 2)
+        location = ((im.size[0] - face.size[0]) // 2 + int(self.center[0] * self.X_FACTOR),
+                    (im.size[1] - face.size[1]) // 2 + int(self.center[1] * self.Y_FACTOR))
         im.paste(face, location)
 
         return im
