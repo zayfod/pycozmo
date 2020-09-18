0bb2d09996c185fee165e853f4ed7e6bcbde6418
Aitor Miguel Blanco <aitormibl@gmail.com>
2020-09-18 16:32:12 +1000

    Added changes to AudioManager to preload audio files before streaming
    Signed-off-by: Aitor Miguel Blanco <aitormibl@gmail.com>

--- pycozmo/audio.py
+++ pycozmo/audio.py
@@ -25,9 +25,9 @@ class AudioManager():
         self.conn = conn
         self.thread = Thread()
         self.lock = Lock()
-        self.audio_gen = []
+        self.audio_stream = []
 
-    def start(self):
+    def start_stream(self):
         self.stop = False
         if not self.thread.is_alive():
             self.thread = Thread(target=self.run, name=__class__.__name__)
@@ -36,62 +36,39 @@ class AudioManager():
     def stop(self) -> None:
         self.stop = True
         self.thread.join()
-        self.audio_gen = []
+        self.audio_stream = []
 
     def run(self) -> None:
-        while len(self.audio_gen) > 0:
-            generator = []
-
+        while len(self.audio_stream) > 0 and not self.stop:
+            next_trigger_time = datetime.now() + timedelta(seconds=MIN_WAIT)
             with self.lock:
-                generator = self.audio_gen.pop(0)
+                pkt = self.audio_stream.pop(0)
+            self.conn.send(pkt)
 
-            next_trigger_time = datetime.now()
-
-            for pkt in generator:
-
-                resting_time = (next_trigger_time - datetime.now()).total_seconds()
-                if resting_time > 0:
-                    time.sleep(resting_time)
-
-                self.conn.send(pkt)
-
-                next_trigger_time = datetime.now() + timedelta(seconds=MIN_WAIT)
-
-                if self.stop:
-                    return
-
-
-    def _play_wav(self, filename: str):
-        with wave.open(filename, "r") as w:
-            sampwidth = w.getsampwidth()
-            ratediv = 2 if w.getframerate() > 30000 else 1
-            channels = w.getnchannels()
-            done = False
-            while not done:
-                frame = bytes_to_cozmo(w.readframes(744 * ratediv),
-                                            sampwidth, ratediv, channels)
-
-                if len(frame) < 744:
-                    frame += [0] * (744 - len(frame))
-                    done = True
-
-                yield OutputAudio(frame)
-
-    def _play_packets(self, packets: list):
-        for p in packets:
-            yield p
+            resting_time = (next_trigger_time - datetime.now()).total_seconds()
+            if resting_time > 0:
+                time.sleep(resting_time)
 
     def play(self, audio):
-        if '.wav' in audio:
-            with self.lock:
-                self.audio_gen.append( self._play_wav(audio) )
-        elif isinstance(audio, list):
-            with self.lock:
-                self.audio_gen.append( self._play_packets(audio) )
+        if audio and isinstance(audio, list) and isinstance(audio[0], OutputAudio):
+                with self.lock:
+                    self.audio_stream += audio
         else:
-            raise TypeError('Invalid audio type: {}'.format(type(audio)))
+            raise TypeError('Invalid audio: {}'.format(type(audio)))
 
-        self.start()
+        self.start_stream()
+
+    def play_file(self, filename):
+        if isinstance(filename, str):
+            if '.wav' == filename[-4:]:
+                self.play(load_wav(filename))
+            else:
+                raise ValueError(
+                    'Only WAV files are supported, invalid audio file: {}'.format(filename))
+        else:
+            raise TypeError('Expected path to audio file, invalid argument: {}'.format(filename))
+
+        self.start_stream()
 
     def wait_until_complete(self):
         self.thread.join()
@@ -100,22 +77,35 @@ class AudioManager():
         return self.thread.is_alive()
 
 
-def bytes_to_cozmo(byte_string: bytes, sampwidth: int, rate_correction: int, channels: int):
+def load_wav(self, filename: str):
+    with wave.open(filename, "r") as w:
+        sampwidth = w.getsampwidth()
+        framerate = w.getframerate()
+        if sampwidth != 2 or (framerate != 22050 and framerate != 48000):
+            raise TypeError('Invalid audio format, only 16 bit samples are supported,' +
+                            'with 22050Hz or 48000Hz frame rates.')
+
+        ratediv = 2 if framerate == 48000 else 1
+        channels = w.getnchannels()
+        done = False
+        pkt_list = []
+
+        while not done:
+            frame = bytes_to_cozmo(w.readframes(744 * ratediv), ratediv, channels)
+            if len(frame) < 744:
+                frame += [0] * (744 - len(frame))
+                done = True
+
+            pkt_list.append(OutputAudio(frame))
+        return pkt_list
+
+
+def bytes_to_cozmo(byte_string: bytes, rate_correction: int, channels: int):
     out = []
     n = channels * rate_correction
-    if sampwidth == 1:
-        # Usually, this will be a signed byte. It needs to be translated to something similar
-        # to ulaw
-        bs = struct.unpack('{}b'.format(len(byte_string)), byte_string)[0::n]
-        for s in bs:
-            out.append(s + 127 if s > 0 else -s)
-    elif sampwidth == 2:
-        bs = struct.unpack('{}h'.format(int(len(byte_string) / 2)), byte_string)[0::n]
-        for s in bs:
-            out.append(u_law_encoding(s))
-    else:
-        raise TypeError('Invalid sample width: {}\nOnly 8 and 16 bytes supported'
-                        .format(sampwidth))
+    bs = struct.unpack('{}h'.format(int(len(byte_string) / 2)), byte_string)[0::n]
+    for s in bs:
+        out.append(u_law_encoding(s))
     return out
 
 
@@ -133,7 +123,7 @@ def u_law_encoding(sample):
     if (sample > MULAW_MAX):
         sample = MULAW_MAX
 
-    while ((sample & mask) != mask and position >= 7):
+    while (sample & mask) != mask and position >= 7:
         mask >>= 1
         position -= 1
 
