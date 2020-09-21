4e1ef42047e9dd02d3bf09478061a9e1c710610f
Kaloyan Tenchov <zayfod@gmail.com>
2020-09-21 12:49:03 -0400

    Member variable case

--- pycozmo/procedural_face.py
+++ pycozmo/procedural_face.py
@@ -30,14 +30,14 @@ class ProceduralBase(object):
     def __init__(self,
                  width: int,
                  height: int):
-        self.WIDTH = int(width)
-        self.HEIGHT = int(height)
-        self.EYE_WIDTH = int(width * (DEFAULT_EYE_WIDTH / DEFAULT_WIDTH))
-        self.EYE_HEIGHT = int(height * (DEFAULT_EYE_HEIGHT / DEFAULT_HEIGHT))
-        self.HALF_EYE_WIDTH = self.EYE_WIDTH // 2
-        self.HALF_EYE_HEIGHT = self.EYE_HEIGHT // 2
-        self.scale_factor_lid_height = int(1.2 * self.EYE_WIDTH)
-        self.scale_factor_lid_bend = int(1.2 * self.HALF_EYE_WIDTH)
+        self.width = int(width)
+        self.height = int(height)
+        self.eye_width = int(width * (DEFAULT_EYE_WIDTH / DEFAULT_WIDTH))
+        self.eye_height = int(height * (DEFAULT_EYE_HEIGHT / DEFAULT_HEIGHT))
+        self.half_eye_width = self.eye_width // 2
+        self.half_eye_height = self.eye_height // 2
+        self.scale_factor_lid_height = int(1.2 * self.eye_width)
+        self.scale_factor_lid_bend = int(1.2 * self.half_eye_width)
 
 
 class ProceduralLid(ProceduralBase):
@@ -57,27 +57,27 @@ class ProceduralLid(ProceduralBase):
         self.y = float(y)
         self.angle = float(angle)
         self.bend = float(bend)
-        self.BLACK = Image.new("1", (self.WIDTH * 2, self.HEIGHT * 2), color=0)
+        self.black = Image.new("1", (self.width * 2, self.height * 2), color=0)
 
     def render(self, im: Image) -> None:
         # Lid image
-        lid = Image.new("1", (self.WIDTH * 2, self.HEIGHT * 2), color=0)
+        lid = Image.new("1", (self.width * 2, self.height * 2), color=0)
 
         draw = ImageDraw.Draw(lid)
 
         # Draw lid
-        lid_height = int(self.EYE_HEIGHT * self.y)
-        x1 = self.WIDTH - self.scale_factor_lid_height
-        y1 = self.HEIGHT - 1 - self.HALF_EYE_HEIGHT
-        x2 = self.WIDTH + self.scale_factor_lid_height
-        y2 = self.HEIGHT - 1 + lid_height
+        lid_height = int(self.eye_height * self.y)
+        x1 = self.width - self.scale_factor_lid_height
+        y1 = self.height - 1 - self.half_eye_height
+        x2 = self.width + self.scale_factor_lid_height
+        y2 = self.height - 1 + lid_height
         draw.rectangle(((x1, y1), (x2, y2)), fill=1)
 
-        bend_height = int(self.EYE_HEIGHT * (1.0 - self.y) * self.bend)
-        x3 = self.WIDTH - self.scale_factor_lid_bend
-        y3 = self.HEIGHT - 1 + lid_height - bend_height
-        x4 = self.WIDTH + self.scale_factor_lid_bend
-        y4 = self.HEIGHT - 1 + lid_height + bend_height
+        bend_height = int(self.eye_height * (1.0 - self.y) * self.bend)
+        x3 = self.width - self.scale_factor_lid_bend
+        y3 = self.height - 1 + lid_height - bend_height
+        x4 = self.width + self.scale_factor_lid_bend
+        y4 = self.height - 1 + lid_height + bend_height
         draw.chord(((x3, y3), (x4, y4)), 0, 180, fill=1)
 
         # Rotate
@@ -86,7 +86,7 @@ class ProceduralLid(ProceduralBase):
         # Translate and compose
         location = ((im.size[0] - lid.size[0]) // 2,
                     (im.size[1] - lid.size[1]) // 2 + self.offset)
-        im.paste(self.BLACK, location, lid)
+        im.paste(self.black, location, lid)
 
 
 class ProceduralEye(ProceduralBase):
@@ -115,9 +115,9 @@ class ProceduralEye(ProceduralBase):
                  width: int = DEFAULT_WIDTH,
                  height: int = DEFAULT_HEIGHT):
         super(ProceduralEye, self).__init__(width, height)
-        self.X_FACTOR = 0.55
-        self.Y_FACTOR = 0.25
-        self.CORNER_RADIUS = (self.WIDTH / 20 + self.HEIGHT / 10)
+        self.x_factor = 0.55
+        self.y_factor = 0.25
+        self.corner_radius = (self.width / 20 + self.height / 10)
         self.offset = int(offset)
         self.center_x = int(center_x)
         self.center_y = int(center_y)
@@ -133,84 +133,84 @@ class ProceduralEye(ProceduralBase):
         self.upper_outer_radius_x = float(upper_outer_radius_x)
         self.upper_outer_radius_y = float(upper_outer_radius_y)
         self.lids = (
-            ProceduralLid(-self.HALF_EYE_HEIGHT, 0.0, upper_lid_y, upper_lid_angle, upper_lid_bend,
-                          self.WIDTH, self.HEIGHT),
-            ProceduralLid(self.HALF_EYE_HEIGHT + 1, 180.0, lower_lid_y, lower_lid_angle, lower_lid_bend,
-                          self.WIDTH, self.HEIGHT)
+            ProceduralLid(-self.half_eye_height, 0.0, upper_lid_y, upper_lid_angle, upper_lid_bend,
+                          self.width, self.height),
+            ProceduralLid(self.half_eye_height + 1, 180.0, lower_lid_y, lower_lid_angle, lower_lid_bend,
+                          self.width, self.height)
         )
 
     def _render_inner_rect(self, draw: ImageDraw, y1: int, x2: int, y2: int) -> None:
-        x3 = x2 - int(self.CORNER_RADIUS * max(self.upper_inner_radius_x, self.lower_inner_radius_x))
-        y3 = y1 + int(self.CORNER_RADIUS * self.upper_inner_radius_y)
+        x3 = x2 - int(self.corner_radius * max(self.upper_inner_radius_x, self.lower_inner_radius_x))
+        y3 = y1 + int(self.corner_radius * self.upper_inner_radius_y)
         x4 = x2
-        y4 = y2 - int(self.CORNER_RADIUS * self.lower_inner_radius_y)
+        y4 = y2 - int(self.corner_radius * self.lower_inner_radius_y)
         draw.rectangle(((x3, y3), (x4, y4)), fill=1)
 
     def _render_upper_rect(self, draw: ImageDraw, x1: int, y1: int, x2: int) -> None:
-        x3 = x1 + int(self.CORNER_RADIUS * self.upper_outer_radius_x)
+        x3 = x1 + int(self.corner_radius * self.upper_outer_radius_x)
         y3 = y1
-        x4 = x2 - int(self.CORNER_RADIUS * self.upper_inner_radius_x)
-        y4 = y1 + int(self.CORNER_RADIUS * max(self.upper_outer_radius_y, self.upper_inner_radius_y))
+        x4 = x2 - int(self.corner_radius * self.upper_inner_radius_x)
+        y4 = y1 + int(self.corner_radius * max(self.upper_outer_radius_y, self.upper_inner_radius_y))
         draw.rectangle(((x3, y3), (x4, y4)), fill=1)
 
     def _render_outer_rect(self, draw: ImageDraw, x1: int, y1: int, y2: int) -> None:
         x3 = x1
-        y3 = y1 + int(self.CORNER_RADIUS * self.upper_outer_radius_y)
-        x4 = x1 + int(self.CORNER_RADIUS * max(self.upper_outer_radius_x, self.lower_outer_radius_x))
-        y4 = y2 - int(self.CORNER_RADIUS * self.lower_outer_radius_y)
+        y3 = y1 + int(self.corner_radius * self.upper_outer_radius_y)
+        x4 = x1 + int(self.corner_radius * max(self.upper_outer_radius_x, self.lower_outer_radius_x))
+        y4 = y2 - int(self.corner_radius * self.lower_outer_radius_y)
         draw.rectangle(((x3, y3), (x4, y4)), fill=1)
 
     def _render_lower_rect(self, draw: ImageDraw, x1: int, x2: int, y2: int) -> None:
-        x3 = x1 + int(self.CORNER_RADIUS * self.lower_outer_radius_x)
-        y3 = y2 - int(self.CORNER_RADIUS * max(self.lower_outer_radius_y, self.lower_inner_radius_y))
-        x4 = x2 - int(self.CORNER_RADIUS * self.lower_inner_radius_x)
+        x3 = x1 + int(self.corner_radius * self.lower_outer_radius_x)
+        y3 = y2 - int(self.corner_radius * max(self.lower_outer_radius_y, self.lower_inner_radius_y))
+        x4 = x2 - int(self.corner_radius * self.lower_inner_radius_x)
         y4 = y2
         draw.rectangle(((x3, y3), (x4, y4)), fill=1)
 
     def _render_center_rect(self, draw: ImageDraw, x1: int, y1: int, x2: int, y2: int) -> None:
-        x3 = x1 + int(self.CORNER_RADIUS * max(self.upper_outer_radius_x, self.lower_outer_radius_x))
-        y3 = y1 + int(self.CORNER_RADIUS * max(self.upper_outer_radius_y, self.upper_inner_radius_y))
-        x4 = x2 - int(self.CORNER_RADIUS * max(self.upper_inner_radius_y, self.lower_inner_radius_y))
-        y4 = y2 - int(self.CORNER_RADIUS * max(self.lower_outer_radius_y, self.lower_inner_radius_y))
+        x3 = x1 + int(self.corner_radius * max(self.upper_outer_radius_x, self.lower_outer_radius_x))
+        y3 = y1 + int(self.corner_radius * max(self.upper_outer_radius_y, self.upper_inner_radius_y))
+        x4 = x2 - int(self.corner_radius * max(self.upper_inner_radius_y, self.lower_inner_radius_y))
+        y4 = y2 - int(self.corner_radius * max(self.lower_outer_radius_y, self.lower_inner_radius_y))
         draw.rectangle(((x3, y3), (x4, y4)), fill=1)
 
     def _render_lower_inner_pie(self, draw: ImageDraw, x2: int, y2: int) -> None:
-        x3 = x2 - 2 * int(self.CORNER_RADIUS * self.lower_inner_radius_x)
-        y3 = y2 - 2 * int(self.CORNER_RADIUS * self.lower_inner_radius_y)
+        x3 = x2 - 2 * int(self.corner_radius * self.lower_inner_radius_x)
+        y3 = y2 - 2 * int(self.corner_radius * self.lower_inner_radius_y)
         x4 = x2
         y4 = y2
         draw.pieslice(((x3, y3), (x4, y4)), 0, 90, fill=1)
 
     def _render_upper_inner_pie(self, draw: ImageDraw, y1: int, x2: int) -> None:
-        x3 = x2 - 2 * int(self.CORNER_RADIUS * self.upper_inner_radius_x)
+        x3 = x2 - 2 * int(self.corner_radius * self.upper_inner_radius_x)
         y3 = y1
         x4 = x2
-        y4 = y1 + 2 * int(self.CORNER_RADIUS * self.upper_inner_radius_y)
+        y4 = y1 + 2 * int(self.corner_radius * self.upper_inner_radius_y)
         draw.pieslice(((x3, y3), (x4, y4)), 270, 360, fill=1)
 
     def _render_upper_outer_pie(self, draw: ImageDraw, x1: int, y1: int) -> None:
         x3 = x1
         y3 = y1
-        x4 = x1 + 2 * int(self.CORNER_RADIUS * self.upper_outer_radius_x)
-        y4 = y1 + 2 * int(self.CORNER_RADIUS * self.upper_outer_radius_y)
+        x4 = x1 + 2 * int(self.corner_radius * self.upper_outer_radius_x)
+        y4 = y1 + 2 * int(self.corner_radius * self.upper_outer_radius_y)
         draw.pieslice(((x3, y3), (x4, y4)), 180, 270, fill=1)
 
     def _render_lower_outer_pie(self, draw: ImageDraw, x1: int, y2: int) -> None:
         x3 = x1
-        y3 = y2 - 2 * int(self.CORNER_RADIUS * self.lower_outer_radius_y)
-        x4 = x1 + 2 * int(self.CORNER_RADIUS * self.lower_outer_radius_x)
+        y3 = y2 - 2 * int(self.corner_radius * self.lower_outer_radius_y)
+        x4 = x1 + 2 * int(self.corner_radius * self.lower_outer_radius_x)
         y4 = y2
         draw.pieslice(((x3, y3), (x4, y4)), 90, 180, fill=1)
 
     def render(self, im: Image) -> None:
         # Eye image
-        eye = Image.new("1", (self.WIDTH, self.HEIGHT), color=0)
+        eye = Image.new("1", (self.width, self.height), color=0)
 
         draw = ImageDraw.Draw(eye)
-        x1 = eye.size[0] // 2 - self.HALF_EYE_WIDTH
-        y1 = eye.size[1] // 2 - self.HALF_EYE_HEIGHT
-        x2 = eye.size[0] // 2 + self.HALF_EYE_WIDTH
-        y2 = eye.size[1] // 2 + self.HALF_EYE_HEIGHT
+        x1 = eye.size[0] // 2 - self.half_eye_width
+        y1 = eye.size[1] // 2 - self.half_eye_height
+        x2 = eye.size[0] // 2 + self.half_eye_width
+        y2 = eye.size[1] // 2 + self.half_eye_height
         self._render_inner_rect(draw, y1, x2, y2)
         self._render_upper_rect(draw, x1, y1, x2)
         self._render_outer_rect(draw, x1, y1, y2)
@@ -239,8 +239,8 @@ class ProceduralEye(ProceduralBase):
 
         # Translate and compose
         if eye:
-            location = ((im.size[0] - eye.size[0]) // 2 + int(self.center_x * self.X_FACTOR) + self.offset,
-                        (im.size[1] - eye.size[1]) // 2 + int(self.center_y * self.Y_FACTOR))
+            location = ((im.size[0] - eye.size[0]) // 2 + int(self.center_x * self.x_factor) + self.offset,
+                        (im.size[1] - eye.size[1]) // 2 + int(self.center_y * self.y_factor))
             im.paste(eye, location, eye)
 
 
@@ -261,8 +261,8 @@ class ProceduralFace(ProceduralBase):
                  height: int = DEFAULT_HEIGHT
                  ):
         super(ProceduralFace, self).__init__(width, height)
-        self.LEFT_EYE_OFFSET = -int(self.WIDTH / 6)
-        self.RIGHT_EYE_OFFSET = int(self.WIDTH / 6)
+        self.left_eye_offset = -int(self.width / 6)
+        self.right_eye_offset = int(self.width / 6)
         self.center_x = int(center_x)
         self.center_y = int(center_y)
         self.scale_x = float(scale_x)
@@ -270,7 +270,7 @@ class ProceduralFace(ProceduralBase):
         self.angle = float(angle)
         if left_eye:
             self.left_eye = ProceduralEye(
-                self.LEFT_EYE_OFFSET,
+                self.left_eye_offset,
                 left_eye[0], left_eye[1],
                 left_eye[2], left_eye[3],
                 left_eye[4],
@@ -280,14 +280,14 @@ class ProceduralFace(ProceduralBase):
                 left_eye[11], left_eye[12],
                 left_eye[13], left_eye[14], left_eye[15],
                 left_eye[16], left_eye[17], left_eye[18],
-                width=self.WIDTH,
-                height=self.HEIGHT
+                width=self.width,
+                height=self.height
             )
         else:
-            self.left_eye = ProceduralEye(self.LEFT_EYE_OFFSET)
+            self.left_eye = ProceduralEye(self.left_eye_offset)
         if right_eye:
             self.right_eye = ProceduralEye(
-                self.RIGHT_EYE_OFFSET,
+                self.right_eye_offset,
                 right_eye[0], right_eye[1],
                 right_eye[2], right_eye[3],
                 right_eye[4],
@@ -297,18 +297,18 @@ class ProceduralFace(ProceduralBase):
                 right_eye[11], right_eye[12],
                 right_eye[13], right_eye[14], right_eye[15],
                 right_eye[16], right_eye[17], right_eye[18],
-                width=self.WIDTH,
-                height=self.HEIGHT
+                width=self.width,
+                height=self.height
             )
         else:
-            self.right_eye = ProceduralEye(self.RIGHT_EYE_OFFSET)
+            self.right_eye = ProceduralEye(self.right_eye_offset)
 
     def render(self) -> Image:
         # Background image
-        im = Image.new("1", (self.WIDTH, self.HEIGHT), color=0)
+        im = Image.new("1", (self.width, self.height), color=0)
 
         # Face image
-        face = Image.new("1", (self.WIDTH, self.HEIGHT), color=0)
+        face = Image.new("1", (self.width, self.height), color=0)
 
         # Draw eyes
         self.left_eye.render(face)
