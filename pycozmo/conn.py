1b3e39d7c2fc0ab1bb5e9f54e3446344dd0d955c
Kaloyan Tenchov <zayfod@gmail.com>
2020-10-24 00:21:23 -0400

    Add send and receive thread counters

--- pycozmo/conn.py
+++ pycozmo/conn.py
@@ -36,6 +36,7 @@ __all__ = [
 ROBOT_ADDR = ("172.31.1.1", 5551)
 PING_INTERVAL = 0.05
 RUN_INTERVAL = 0.05
+STATS_INTERVAL = 60.0
 
 
 class SendThread(Thread):
@@ -56,6 +57,16 @@ class SendThread(Thread):
         self.queue_timeout = queue_timeout
         self.last_ack = 0
         self.last_send_timestamp = datetime.now() - timedelta(days=1)
+        # Number of packets received from the application layer.
+        self.outgoing_packets = 0
+        # Number of packets sent (includes resends).
+        self.sent_packets = 0
+        # Number of frames sent.
+        self.sent_frames = 0
+        # Number of discarded frames (e.g. socket buffer overruns).
+        self.discarded_frames = 0
+        # Number of bytes sent.
+        self.sent_bytes = 0
 
     def stop(self) -> None:
         self.stop_flag = True
@@ -67,6 +78,7 @@ class SendThread(Thread):
                 try:
                     pkt = self.queue.get(timeout=self.queue_timeout)
                     self.queue.task_done()
+                    self.outgoing_packets += 1
                     if isinstance(pkt, protocol_encoder.Ping):
                         raw_frame = Frame(
                             protocol_declaration.FrameType.PING, OOB_SEQ, OOB_SEQ, self.last_ack, [pkt]).to_bytes()
@@ -96,6 +108,7 @@ class SendThread(Thread):
                     framelen += len(p) + 1
                     if first_seq is None:
                         first_seq = seq
+                    self.sent_packets += 1
                     if framelen < MAX_FRAME_PAYLOAD_SIZE:
                         to_frame.append(p)
                     else:
@@ -103,8 +116,8 @@ class SendThread(Thread):
                         to_frame = []
                         framelen = 0
                         first_seq = None
-                    if len(raw_frames) >= 3:
-                        break
+                        if len(raw_frames) >= 3:
+                            break
                 if len(to_frame):
                     raw_frames.append(self._build_frame(to_frame, first_seq, seq, last_ack))
 
@@ -118,8 +131,11 @@ class SendThread(Thread):
     def _send_frame(self, raw_frame: bytes) -> None:
         try:
             self.sock.sendto(raw_frame, self.receiver_address)
+            self.sent_frames += 1
+            self.sent_bytes += len(raw_frame)
         except Exception as e:
             logger.error("sendto() failed. {}".format(e))
+            self.discarded_frames += 1
 
     @staticmethod
     def _build_frame(pkts: list, first_seq: int, seq: int, ack: int):
@@ -142,6 +158,11 @@ class SendThread(Thread):
         self.window.reset()
         self.last_ack = 0
         self.last_send_timestamp = datetime.now() - timedelta(days=1)
+        self.outgoing_packets = 0
+        self.sent_packets = 0
+        self.sent_frames = 0
+        self.discarded_frames = 0
+        self.sent_bytes = 0
 
 
 class ReceiveThread(Thread):
@@ -150,17 +171,25 @@ class ReceiveThread(Thread):
                  sock: socket.socket,
                  send_thread: SendThread,
                  sender_address: Optional[Tuple[str, int]],
-                 timeout: float = 0.5,
                  buffer_size: int = 2048) -> None:
         super().__init__(daemon=True, name=__class__.__name__)
         self.sock = sock
         self.sender_address = sender_address
         self.window = ReceiveWindow(16, size=62, max_seq=MAX_SEQ)
-        self.timeout = timeout
         self.buffer_size = buffer_size
         self.stop_flag = False
         self.queue = Queue()
         self.send_thread = send_thread
+        # Received bytes.
+        self.received_bytes = 0
+        # Number of discarded frames (e.g. unexpected source, decode failures, etc.).
+        self.discarded_frames = 0
+        # Number of received frames.
+        self.received_frames = 0
+        # Number of received packets (including duplicates, out-of-order, etc.).
+        self.received_packets = 0
+        # Number of packets, delivered to the application layer.
+        self.delivered_packets = 0
 
     def stop(self) -> None:
         self.stop_flag = True
@@ -169,21 +198,25 @@ class ReceiveThread(Thread):
     def run(self) -> None:
         while not self.stop_flag:
             try:
-                ready = select.select([self.sock], [], [], self.timeout)
+                ready = select.select((self.sock,), (), (), 0.5)
                 if not ready[0]:
                     continue
 
                 raw_frame, address = self.sock.recvfrom(self.buffer_size)
+                self.received_bytes += len(raw_frame)
                 if self.sender_address and self.sender_address != address:
+                    self.discarded_frames += 1
                     logger_protocol.debug("Received a UDP datagram from unexpected address {}.".format(address))
                     continue
             except Exception as e:
+                self.discarded_frames += 1
                 logger.error("Failed to receive frame. {}".format(e))
                 continue
 
             try:
                 frame = Frame.from_bytes(raw_frame)
             except Exception as e:
+                self.discarded_frames += 1
                 logger_protocol.error("Failed to decode frame. {}".format(e))
                 continue
 
@@ -194,14 +227,16 @@ class ReceiveThread(Thread):
                 continue
 
     def handle_frame(self, frame: Frame) -> None:
+        self.received_frames += 1
         self.send_thread.ack(frame.ack, frame.seq)
         for pkt in frame.pkts:
             self.handle_pkt(pkt)
         self.deliver_sequence()
 
     def handle_pkt(self, pkt: Packet) -> None:
+        self.received_packets += 1
         if pkt.is_oob():
-            self.queue.put(pkt)
+            self.deliver(pkt)
         else:
             self.window.put(pkt.seq, pkt)
 
@@ -210,10 +245,19 @@ class ReceiveThread(Thread):
             pkt = self.window.get()
             if pkt is None:
                 break
-            self.queue.put(pkt)
+            self.deliver(pkt)
+
+    def deliver(self, pkt: Packet):
+        self.delivered_packets += 1
+        self.queue.put(pkt)
 
     def reset(self):
         self.window.reset()
+        self.received_bytes = 0
+        self.discarded_frames = 0
+        self.received_frames = 0
+        self.received_packets = 0
+        self.delivered_packets = 0
 
 
 class ClientConnection(Thread, event.Dispatcher):
@@ -242,6 +286,8 @@ class ClientConnection(Thread, event.Dispatcher):
         self.recv_thread = ReceiveThread(self.sock, self.send_thread, self.robot_addr)
         self.stop_flag = False
         self.send_last = datetime.now() - timedelta(days=1)
+        self.ping_last = datetime.now() - timedelta(days=1)
+        self.stats_last = datetime.now()
 
     def start(self) -> None:
         logger.debug("Starting...")
@@ -273,8 +319,10 @@ class ClientConnection(Thread, event.Dispatcher):
 
             if self.state == self.CONNECTED:
                 now = datetime.now()
-                if now - self.send_last > timedelta(seconds=PING_INTERVAL):
+                if now - self.ping_last > timedelta(seconds=PING_INTERVAL):
                     self._send_ping()
+                if now - self.stats_last > timedelta(seconds=STATS_INTERVAL):
+                    self.log_stats()
 
             if pkt is not None:
                 if not self.packet_type_filter.filter(pkt.type.value) and not self.packet_id_filter.filter(pkt.id):
@@ -313,6 +361,7 @@ class ClientConnection(Thread, event.Dispatcher):
         self.send(pkt)
 
     def _send_ping(self) -> None:
+        self.ping_last = datetime.now()
         pkt = protocol_encoder.Ping(0, 1, 0)
         self.send(pkt)
 
@@ -325,3 +374,20 @@ class ClientConnection(Thread, event.Dispatcher):
     def _on_ping(cli, pkt: protocol_encoder.Ping):
         del cli, pkt
         # TODO: Calculate round-trip time
+
+    def log_stats(self):
+        self.stats_last = datetime.now()
+        logger_protocol.info("Recv: {}B, {}F (disc.), {}F, {}P, {}P ({:.02f}%); "
+                             "Sent: {}P, {}P ({:.02f}%), {}F, {}F (disc.), {}B;".format(
+                                self.recv_thread.received_bytes,
+                                self.recv_thread.discarded_frames,
+                                self.recv_thread.received_frames,
+                                self.recv_thread.received_packets,
+                                self.recv_thread.delivered_packets,
+                                self.recv_thread.delivered_packets / (self.recv_thread.received_packets or 1) * 100.0,
+                                self.send_thread.outgoing_packets,
+                                self.send_thread.sent_packets,
+                                self.send_thread.outgoing_packets / (self.send_thread.sent_packets or 1) * 100.0,
+                                self.send_thread.sent_frames,
+                                self.send_thread.discarded_frames,
+                                self.send_thread.sent_bytes))
